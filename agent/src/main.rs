bbba07424577bb30ef7bf31fb427f78b776b48b5
Yassin <yassinwalid@aucegypt.edu>
2022-11-12 22:40:02 +0200

    has an error regarding moving variables, which is stopping the testing of the round robin

--- agent/src/main.rs
+++ agent/src/main.rs
@@ -1,5 +1,5 @@
 use std::thread;
-use std::collections::HashMap;
+use std::net::UdpSocket;
 
 fn main() {
     //will change to external IP later
@@ -8,25 +8,43 @@ fn main() {
     let socket_internal = UdpSocket::bind("127.0.0.1:4245").expect("couldn't bind to address");
 
     let listener_addresses = ["127.0.0.1:4242", "127.0.0.1:4243", "127.0.0.1:4244"];
-    let mut requests_map=HashMap::new();
-    let i=0;
+    let mut i=0;
     loop{
         let mut buf = [0;3];
-        let (number_of_bytes, sender_address) = socket_internal.recv_from(&mut buf)
+        let (number_of_bytes, client_address) = socket_internal.recv_from(&mut buf)
                                                 .expect("Didn't receive data");
-        let temp = thread::spawn(move || {
-            //we need to append port number of client to buf some way or another
-
-            socket_external.send_to(&buf, listener_addresses[i%3].to_string()).expect("couldn't send data");
+        thread::spawn(move || {
+            let client_port_num = client_address.port();
+            let mut buf_agent = [0;5];
+            for j in 0..3 {
+                buf_agent[j]=buf[j];
+            }
+            buf_agent[4] = client_port_num as u8;       //LSB
+            buf_agent[3] = (client_port_num>>8) as u8;  //MSB
+            // socket_external.send_to(&buf_agent, listener_addresses[i%3].to_string()).expect("couldn't send data");
+            println!("i: {}",i);
+            println!("Arr: {:?}",filled_buf);
             i += 1;
             i %= 3;
-            let mut buf = [0;4];
-            let (number_of_bytes, _) = socket_external.recv_from(&mut buf)
-                                            .expect("Didn't receive data");
-            //We need to find a way to use the port number in buf
-            //alongside localhost IP in order to use instead of placeholder below
-            socket_internal.send_to(&buf, /*<placeholder>*/).expect("couldn't send data");
         });
         
     }
 }
+
+// We need to fork the client into two porgrams, one for the recieving requests from clinets and
+// forwarding to servers (see above). The other is for recieving replies from servers and 
+// forwarding to correct clients (see below).
+
+// Each will have an ifinite loop that will be waiting on a recieve on its internal/external
+// sockets respectively.
+
+/*
+let mut buf_agent_reply = [0;5];
+let (number_of_bytes, _) = socket_external.recv_from(&mut buf_agent_reply)
+                                .expect("Didn't receive data");
+//We need to find a way to use the port number in buf
+//alongside localhost IP in order to use instead of placeholder below
+let client_port: u16 = ((buf_agent_reply[3] as u16) << 8) | (buf_agent_reply[4] as u16);
+let socket_address_client_reply = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), client_port);
+socket_internal.send_to(&buf_agent_reply, socket_address_client_reply).expect("couldn't send data");
+*/
---
+++ agent/target/debug/deps/agent-d0e7f28d53c7eb75.d
@@ -0,0 +1,5 @@
+/home/yassin/Desktop/test_conn/Distributed-System-Project/agent/target/debug/deps/agent-d0e7f28d53c7eb75: src/main.rs
+
+/home/yassin/Desktop/test_conn/Distributed-System-Project/agent/target/debug/deps/agent-d0e7f28d53c7eb75.d: src/main.rs
+
+src/main.rs:
