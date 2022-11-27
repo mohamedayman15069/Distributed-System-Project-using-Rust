4694dff951c42484945b290448ea8728ee41a3cb
Yassin <yassinwalid@aucegypt.edu>
2022-11-27 15:41:32 +0200

    Added multithreading to agent, need to add pinging functionality to agent

--- listener2/src/main.rs
+++ listener2/src/main.rs
@@ -3,21 +3,28 @@ fn main() {
 
 	let socket = UdpSocket::bind("127.0.0.1:4243").expect("couldn't bind to address");
     
-    let mut buf = [0;3];
+    let mut buf = [0;5];
 	loop{
 		let (number_of_bytes, src_addr) = socket.recv_from(&mut buf)
                                             .expect("Didn't receive data");
 
         // define a string that is the same length as the received data
-        let filled_buf = &mut buf[..number_of_bytes];
+        let filled_buf = &mut buf[..3];
         // print the received string
         println!("Arr: {:?}",filled_buf);
         // reverse the string
         filled_buf.reverse();
         // print the reversed string
         println!("Arr: {:?}",filled_buf);
+        let mut full_buf = [0;5];
+        for i in 0..3{
+            full_buf[i] = filled_buf[i];
+        }
+        for i in 3..5{
+            full_buf[i] = buf[i];
+        }
         // send the reversed string back to the sender
-        socket.send_to(& filled_buf, src_addr).expect("couldn't send data");
+        socket.send_to(& full_buf, src_addr).expect("couldn't send data");
     }
 }
                                             
